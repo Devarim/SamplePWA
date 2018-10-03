33773258625d84113b10d36f8390440cc0d646b9
Francisco Eduardo Ferreira <devarim.duth@gmail.com>
2018-10-03 20:29:02 -0300

    Update app.js

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('dist/sw.js')
+    navigator.serviceWorker.register('dist/sw.js',{ scope: '/SamplePWA/dist/' })
         .then(function (registration) {
             console.log('Registration successful, scope is:', registration.scope);
         })
